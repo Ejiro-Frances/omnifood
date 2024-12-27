2e57ce7439c1493507a15dc135af8e9c431db20b
Ejiro-Frances <francesejiro@yahoo.com>
2024-12-27 21:37:54 +0100

    final changes

--- js/script.js
+++ js/script.js
@@ -12,3 +12,78 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+// Assuming scroll behaviour doesn't work properly on safari.
+/////////////////////////////////////https://buildkite.com/pinterest
+// Smooth scrolling animation
+///////////////////////////////////////////https://buildkite.com/pinterest
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    // const targetSection = document.querySelector(link.getAttribute("section"));
+
+    // scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelectorAll(href);
+      sectionEl.forEach(function (section) {
+        section.scrollIntoView({ behavior: "smooth" });
+      });
+    }
+
+    // close mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
+
+////////////////////////////////////////https://buildkite.com/pinterest
+// Sticky navigation
+const heroSectionEl = document.querySelector(".hero-container");
+
+const observer = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    } else {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+observer.observe(heroSectionEl);
+
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
